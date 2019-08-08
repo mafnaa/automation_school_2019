07b57f3923928a1f5bf3b0f7774252a53cf881e8
Maxim Fominih <Maxim_Fominih@rcomext.com>
2019-08-08 11:16:47 +0300

    lesson 2 done

---
+++ Lesson2/lesson2.java
@@ -0,0 +1,14 @@
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+public class lesson2 {
+	public static void main(String[] args) {
+		String name = args[0];
+		String surname = args[1];
+		SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MMM-YYYY, E, HH:mm:ss");
+		String date = dateFormat.format(new Date());
+		
+		System.out.println((String.format("%s %s, lesson 2 is finished.", name, surname)));
+		System.out.println(date);
+	}
+}
